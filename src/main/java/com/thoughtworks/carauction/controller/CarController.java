94b4bdf7bd5e6e88febb734ebf45b9800dd5b54e
Grace <gracem@thoughtworks.com>
2016-08-23 11:36:08 +0200

    Grace: change the package name

--- src/main/java/com/thoughtworks/carauction/controller/CarController.java
+++ src/main/java/com/thoughtworks/carauction/controller/CarController.java
@@ -1,4 +1,4 @@
-package com.thoughtworks.carauction;
+package com.thoughtworks.carauction.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
