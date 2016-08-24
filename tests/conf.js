8b4a063b3b9ae57f567ca1eab3f9a55878222de6
Felix Moalusi <felixsean@ymail.com>
2016-08-24 14:10:37 +0200

    protractor setup

--- tests/conf.js
+++ tests/conf.js
@@ -2,3 +2,5 @@ exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['todo-spec.js']
 };
+if (process.env.SNAP_CI) {
+  exports.config.chromeDriver = '/usr/local/bin/chromedriver';
